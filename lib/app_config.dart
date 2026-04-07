17e6ba55f0059bc7172f322c266d8976fc7eb670
Julio Vela de Sosa <75851675+juliovela1986@users.noreply.github.com>
2026-04-08 01:42:08 +0200

    chore(config): update local backend and keycloak endpoints

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,8 +1,13 @@
 class AppConfig {
-  //static const String keycloakUrl = 'http://julio86.myqnapcloud.com:8180';
-  //static const String backendUrl = 'http://julio86.myqnapcloud.com:8081';
-  static const String backendUrl = 'http://192.168.1.143:8081';
-  static const String keycloakUrl = 'http://192.168.1.143:8180';
+  //static const String backendUrl = 'http://192.168.1.143:8081';
+  //static const String keycloakUrl = 'http://192.168.1.143:8180';
+  //static const String keycloakUrl = 'https://julio86.myqnapcloud.com';
+  //static const String backendUrl = 'https://julio86.myqnapcloud.com:8444';
+// 1. Tu API de Spring Boot a través del Proxy HTTPS
+static const String backendUrl = 'https://api-gestioncomida.duckdns.org';
+// 2. Tu Servidor de Autenticación Keycloak a través del Proxy HTTPS
+static const String keycloakUrl = 'https://auth-gestioncomida.duckdns.org';
+
   static String get keycloakRealm => '$keycloakUrl/realms/gestioncomida';
   static String get discoveryUrl => '$keycloakRealm/.well-known/openid-configuration';
 }
